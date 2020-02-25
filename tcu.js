a073869ee9be93af412d4c2a19d850c377e98fe7
Joe Jackson <jbudorg@gmail.com>
2020-02-24 21:31:34 -0800

    some shift code

--- tcu.js
+++ tcu.js
@@ -1,9 +1,9 @@
 const clutch = new Clutch();
-const gears = new Gears();
+const gear = new Gears();
 const ecu = new Ecu();
 const canbus = new Canbus();
 //Gather data from ecu
-const Ecudata = {}
+const Ecudata = {};
 
 async function datastream(){
   Ecudata = {
@@ -23,28 +23,63 @@ let automatic = true;
 let upshiftpoints = [];
 let downshiftpoints = [];
 
+let mode = NORMAL;
 let modes = {
   MANUAL = {
     () => automatic = false
   },
-  NORMAL = {},
-  SPORT = {},
+  NORMAL = {
+    // todo: shiftpoints for normal
+
+  },
+  SPORT = {
+     // todo: shiftpoints for sport
+  },
   ECO = {
-    for (let g in gears.gears) {
-      upshiftpoints.g = gears.minspeeds.g;
-      downshiftpoints.g = gears.minspeeds.g;
+      () => {
+        for (let g in gears.gear) {
+          upshiftpoints.g = gear.minspeeds.g;
+          downshiftpoints.g = gear.minspeeds.g;
+      }
     }
-
   }
+}
 
+function setMode(m){
+  modes.m(); // execute the mode's code
+  mode = modes.m; // set the enumeration for the mode
 }
 
 function setAuto(toggle){
   automatic = toggle ? true : false;
 }
 
+// automatic shift
+function ashift(from, to) {
+    switch(mode){
+      case SPORT:
+        await clutch.engageLarp(0.9);
+        if (from > to) { // downshift
+          await gear.shift(-1); // neutral
+          await clutch.disengageLarp(0.9);
 
-function shift(from, to) {
+          await canbus.sendData('Tpos', (from, to)=>
+              gear.calculateRevMatch(from, to);
+            ).until(Ecudata.CURRENTRPM == gear.calculateRevMatch(from, to));
+          await clutch.engage();
+        }
+        await gear.shift(to);
+        await clutch.disengageLarp(0.825);
 
+      default:
+        clutch.engageLarp(0.425);
+        await gear.shift(to);
+        clutch.disengageLarp(0.425);
+
+    }
+}
+
+function mshift() {
+// TODO: just pipe the inputs directly from the shifter sensor and clutch sensors
 
 }
